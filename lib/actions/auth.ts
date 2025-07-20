f469a48c5d52ed98ce7e13b8d6b2de5686d23d64
utshavnepal <utshavnepalofficial@gmail.com>
2025-07-20 23:15:15 +0530

    first commit

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -6,24 +6,20 @@ import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-
+import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
-
+import { workflowClient } from "@/lib/workflow";
 import config from "@/lib/config";
-import ratelimit from "../ratelimit";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
 ) => {
   const { email, password } = params;
 
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
 
-
-const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1"
-const {success} = await ratelimit.limit(ip)
-
-  if(!success) return redirect('/too-fast')
- 
+  if (!success) return redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -43,15 +39,13 @@ const {success} = await ratelimit.limit(ip)
   }
 };
 
-
-
 export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, universityId, password, universityCard } = params;
 
-const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1"
-const {success} = await ratelimit.limit(ip)
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
 
-  if(!success) return redirect('/too-fast')
+  if (!success) return redirect("/too-fast");
 
   const existingUser = await db
     .select()
@@ -74,7 +68,13 @@ const {success} = await ratelimit.limit(ip)
       universityCard,
     });
 
-   
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
+    });
 
     await signInWithCredentials({ email, password });
 
