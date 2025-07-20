cc4fe77948656fa1594adbcbfcab3c9a5ece579d
utshavnepal <utshavnepalofficial@gmail.com>
2025-07-21 00:10:51 +0530

    first commit

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,93 +1,194 @@
-"use client"
-import React, { useRef, useState } from 'react'
-import {IKImage, ImageKitProvider, IKUpload} from 'imagekitio-next'
-import config from '@/lib/config'
-import Image from 'next/image'
-import { toast } from 'sonner'
+"use client";
+
+import { IKImage, ImageKitProvider, IKUpload, IKVideo } from "imagekitio-next";
+import config from "@/lib/config";
+import ImageKit from "imagekit";
+import { useRef, useState } from "react";
+import Image from "next/image";
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
 
 const {
-    env:{
-        imagekit:{publicKey, privateKey, urlEndpoint}
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`,
+      );
     }
-} = config
 
-  
-const ImageUpload = ({onFileChange}:{onFileChange:(filePath:string)=>void}) => {
+    const data = await response.json();
 
-const ikUploadRef = useRef(null)
-const [file, setFile] = useState<{filePath:string}|null>(null)
-const authentitator = async ()=>{
-    try {
-        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
-        if(!response.ok) {
-            const errorText = await response.text();
-            throw new Error(
-                `Request failed with status ${response.status}:${errorText}`
-            )
-        }
+    const { signature, expire, token } = data;
 
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
 
-   const data = await response.json();
-     const {signature, expire, token} = data
-     return {token, expire, signature}
+interface Props {
+  type: "image" | "video";
+  accept: string;
+  placeholder: string;
+  folder: string;
+  variant: "dark" | "light";
+  onFileChange: (filePath: string) => void;
+  value?: string;
+}
 
-    } catch (error:any) {
-        throw new Error(`authentication failed : ${error.message}`)
+const FileUpload = ({
+  type,
+  accept,
+  placeholder,
+  folder,
+  variant,
+  onFileChange,
+  value,
+}: Props) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string | null }>({
+    filePath: value ?? null,
+  });
+  const [progress, setProgress] = useState(0);
+
+  const styles = {
+    button:
+      variant === "dark"
+        ? "bg-dark-300"
+        : "bg-light-600 border-gray-100 border",
+    placeholder: variant === "dark" ? "text-light-100" : "text-slate-500",
+    text: variant === "dark" ? "text-light-100" : "text-dark-400",
+  };
+
+  const onError = (error: any) => {
+    console.log(error);
+
+    toast({
+      title: `${type} upload failed`,
+      description: `Your ${type} could not be uploaded. Please try again.`,
+      variant: "destructive",
+    });
+  };
+
+  const onSuccess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+
+    toast({
+      title: `${type} uploaded successfully`,
+      description: `${res.filePath} uploaded successfully!`,
+    });
+  };
+
+  const onValidate = (file: File) => {
+    if (type === "image") {
+      if (file.size > 20 * 1024 * 1024) {
+        toast({
+          title: "File size too large",
+          description: "Please upload a file that is less than 20MB in size",
+          variant: "destructive",
+        });
+
+        return false;
+      }
+    } else if (type === "video") {
+      if (file.size > 50 * 1024 * 1024) {
+        toast({
+          title: "File size too large",
+          description: "Please upload a file that is less than 50MB in size",
+          variant: "destructive",
+        });
+        return false;
+      }
     }
-}
 
-const onError =(error:any)=>{
-toast.message('Event has been failed', {
-  description: `${error} uploaded sucessfully`,
-})
-}
-const onSucess = (res:any)=>{
-setFile(res)
-
-onFileChange(res.filePath)
-toast.message('Image has been Uploaded', {
-  description: `${res.filePath} uploaded sucessfully`,
-})
-
-}
-
+    return true;
+  };
 
   return (
-    <ImageKitProvider urlEndpoint={urlEndpoint} publicKey={publicKey} authenticator={authentitator} >
-        <IKUpload
-        className='hidden'
-        ref = {ikUploadRef}
-    onError={onError}
-    onSuccess={onSucess}
-    fileName='test-upload.png'
-    />
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        useUniqueFileName={true}
+        validateFile={onValidate}
+        onUploadStart={() => setProgress(0)}
+        onUploadProgress={({ loaded, total }) => {
+          const percent = Math.round((loaded / total) * 100);
 
-<button className='flex min-h-14 w-full items-center justify-center gap-1.5 rounded-md' onClick={(e) => {
+          setProgress(percent);
+        }}
+        folder={folder}
+        accept={accept}
+        className="hidden"
+      />
+
+      <button
+        className={cn("upload-btn", styles.button)}
+        onClick={(e) => {
           e.preventDefault();
 
           if (ikUploadRef.current) {
             // @ts-ignore
             ikUploadRef.current?.click();
           }
-        }}>
-    <Image src='/icons/upload.svg' alt='upload' width={20} height={20} className='object-contain'/>
-    <p className='text-base text-lime-100'>
-        upload a File
-    </p>
-    {file && <p className='mt-1 text-center text-xs'>
-    {file.filePath}
-    </p>}
-</button>
-{file && (
-    <IKImage
-    alt={file.filePath}
-    path={file.filePath}
-    width={500}
-    height={500}
-    />
-)}
-    </ImageKitProvider>
-  )
-}
+        }}
+      >
+        <Image
+          src="/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
 
-export default ImageUpload
+        <p className={cn("text-base", styles.placeholder)}>{placeholder}</p>
+
+        {file && (
+          <p className={cn("upload-filename", styles.text)}>{file.filePath}</p>
+        )}
+      </button>
+
+      {progress > 0 && progress !== 100 && (
+        <div className="w-full rounded-full bg-green-200">
+          <div className="progress" style={{ width: `${progress}%` }}>
+            {progress}%
+          </div>
+        </div>
+      )}
+
+      {file &&
+        (type === "image" ? (
+          <IKImage
+            alt={file.filePath}
+            path={file.filePath}
+            width={500}
+            height={300}
+          />
+        ) : type === "video" ? (
+          <IKVideo
+            path={file.filePath}
+            controls={true}
+            className="h-96 w-full rounded-xl"
+          />
+        ) : null)}
+    </ImageKitProvider>
+  );
+};
+
+export default FileUpload;
